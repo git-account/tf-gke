7c59edb2642c1daeefe29c523c04f40fe55f8c42
Ivan Ryzhkov <ivan.ryzhkow@gmail.com>
2026-01-09 16:38:52 +0000

    fix something

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "gke_cluster" {
   source         = "github.com/git-account/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  google_region  = var.google_region
+  google_project = var.google_project
+  gke_num_nodes  = var.gke_num_nodes
 }
